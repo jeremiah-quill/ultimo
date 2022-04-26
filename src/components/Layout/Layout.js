446bcc0db73277749a8fd3428ba9e5db4b5dec1d
Jeremiah Quill <jcq5010@gmail.com>
2022-04-26 08:09:58 -0400

    fixed layout file capitilization

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Footer from '../footer';
+import DesktopNavbar from '../DesktopNavbar';
+import useIsMobile from '../../hooks/useIsMobile';
+import '../../styles/main.scss';
+
+const Layout = ({ children }) => {
+  const [isMobile] = useIsMobile();
+
+  return (
+    <>
+      {isMobile ? <Navbar /> : <DesktopNavbar />}
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Locations from '../components/Locations';
 import ShopAround from '../components/ShopAround';
