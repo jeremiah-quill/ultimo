c480f20d3d35d4c84a01d5d3f14eb194c1bd4942
Jeremiah Quill <jcq5010@gmail.com>
2022-04-26 09:40:36 -0400

    changed footer to Footer in imports of Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import Footer from '../footer';
+import Footer from '../Footer';
 import DesktopNavbar from '../DesktopNavbar';
 import useIsMobile from '../../hooks/useIsMobile';
 import '../../styles/main.scss';
